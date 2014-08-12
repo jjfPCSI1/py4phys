70f6c7988ce924ccd8bae8c6a78ffa7cd482d084
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2014-08-12 21:10:23 +0200

    Début oscillateur de Landau

--- lib/M4_oscillateur_de_landau_portrait_de_phase.py
+++ lib/M4_oscillateur_de_landau_portrait_de_phase.py
@@ -12,3 +12,20 @@
 # Il faudra alors modifier la première ligne en # coding: utf8
 # pour que Python s'y retrouve.
 
+
+
+
+import numpy as np
+import scipy as sp
+import scipy.integrate
+from portrait_de_phase import portrait_de_phase
+
+def landau(y,t):
+    x,vx = y
+    k,m,d,ell0 = 1,1,3,5
+    return [vx,-k*x/m*(np.sqrt(d**2+x**2) - ell0)/(np.sqrt(d**2+x**2))]
+
+
+
+
+
