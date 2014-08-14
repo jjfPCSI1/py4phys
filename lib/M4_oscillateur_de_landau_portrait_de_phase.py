9c6795067c46b833ffe8ee2aae37c290cf96feab
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2014-08-14 20:29:32 +0200

    Ajout commentaires et Landau tilté

--- lib/M4_oscillateur_de_landau_portrait_de_phase.py
+++ lib/M4_oscillateur_de_landau_portrait_de_phase.py
@@ -15,58 +15,64 @@
 
 
 
-import numpy as np
-import scipy as sp
-import scipy.integrate
-import matplotlib.pyplot as plt
+""" 
+Portrait de phase d'un oscillateur de Landau (bille libre de se déplacer sur 
+une barre horizontale retenue par un ressort accroché à une distance d à la 
+verticale de l'origine).
+"""
+
+import numpy as np                # Les boîtes
+import scipy as sp                # à outils
+import scipy.integrate            # numériques
+import matplotlib.pyplot as plt   # La boîte à outils graphiques
+# Pour les portraits de phase
 from portrait_de_phase import portrait_de_phase,diagramme_energetique
 
-tmax = 40
-nb_points = 2000
-x0 = np.arange(-8,8.1,0.79999999)
-v0 = np.array([0]*len(x0))
-k,m,d,ell0 = 1,1,3,5
+tmax = 40                         # Temps d'intégration
+nb_points = 2000                  # Nb de point pour l'échantillonnage en temps
+x0 = np.arange(-8,8.1,0.79999999) # Positions initiales
+v0 = np.array([0]*len(x0))        # Vitesses initiales (nulles)
+k,m,d,ell0 = 1,1,3,5              # Quelques constantes
 
 colors = ["blue","red","green","magenta","cyan","yellow",
           "darkblue","darkred","darkgreen","darkmagenta","darkcyan"]*10
 
-def landau(y,t):
+def landau(y,t):                  # Équation d'évolution
     x,v = y
     return [v,-k*x/m*(np.sqrt(d**2+x**2) - ell0)/(np.sqrt(d**2+x**2))]
 
-def Ep(x,v):
+def Em(x,v):                      # Énergie mécanique
     return 0.5*k*(np.sqrt(x**2+d**2) - ell0)**2 + 0.5*m*v**2
 
-t = np.linspace(0,tmax,nb_points)
-x,v = [],[]
-print(x0,v0)
-for xi,vi in zip(x0,v0):
-    print(xi,vi)
-    sol = sp.integrate.odeint(landau,[xi,vi],t)
-    x.append(sol[:,0])
-    v.append(sol[:,1])
+t = np.linspace(0,tmax,nb_points) # Échantillonnage en temps
+x,v = [],[]                       # Initialisation
+for xi,vi in zip(x0,v0):          # Itération sur les conditions initiales
+    print(xi,vi)                  # Un peu de feedback
+    sol = sp.integrate.odeint(landau,[xi,vi],t) # On intègre
+    x.append(sol[:,0])            # et on stocke à la fois les positions
+    v.append(sol[:,1])            # et les vitesses
 
-fig = plt.figure(figsize=(10,10))
+fig = plt.figure(figsize=(10,10)) # Création de la figure
 
-vlim = (np.min(v),np.max(v))
+vlim = (np.min(v),np.max(v))      # Limites verticales (horizontales imposées par les CI)
 base_name='PNG/M4_oscillateur_de_landau_portrait_de_phase'
 
-for i,ti in enumerate(t):
-    print(ti)
-    xi = [xp[:i+1] for xp in x]
-    vi = [vp[:i+1] for vp in v]
+for i,ti in enumerate(t):         # On regarde à chaque instant d'intégration
+    print(ti)                     # Un peu de feedback
+    xi = [xp[:i+1] for xp in x]   # On stocke l'avancement
+    vi = [vp[:i+1] for vp in v]   # jusqu'à l'instant présent
     plt.suptitle('Oscillateur de Landau, $t={}$'.format(round(ti,2)))
-    plt.subplot(2,1,1)
+    plt.subplot(2,1,1)            # Première sous-figure
     portrait_de_phase(xi,vi,fantome=50,clearfig=False,color=colors,ylim=vlim)
     plt.xlabel('')
-    plt.subplot(2,1,2)
-    diagramme_energetique(xi,vi,Ep,color=colors,clearfig=False,fantome=50)
+    plt.subplot(2,1,2)            # Seconde sous-figure
+    diagramme_energetique(xi,vi,Em,color=colors,clearfig=False,fantome=50)
     plt.savefig('{}_{:04d}.png'.format(base_name,i))
     plt.clf()
 
-from film import make_film
+from film import make_film        # Boîte à outil visuelle
 
-make_film(base_name)
+make_film(base_name)              # et fabrication du film correspondant
 
 
 
