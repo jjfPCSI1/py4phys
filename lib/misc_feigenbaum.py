edb1508cd77e7a79bd3add2adcd3782c4f9d82a2
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2014-08-17 15:41:11 +0200

    Version fonctionnelle

--- lib/misc_feigenbaum.py
+++ lib/misc_feigenbaum.py
@@ -21,6 +21,47 @@ limites de suites récursives à partir de l'application logistique
 x -> k*x*(1-x).
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import film
+
+kmin,kmax = 2.9,4
+ylim = None
+# À essayer aussi:
+# kmin = 3.9055
+# kmax = 3.9068
+# ylim = (0.48,0.52)
+
+nb_points = 1000
+k_list = np.linspace(kmin,kmax,nb_points)
+iterations_avant_cycle = 1000
+iterations_dans_cycle  = 100
+u0 = 0.1
+
+
+
+def f(x,k):
+    return k*x*(1-x)
+
+def get_cycle(k,u0):
+    avant = np.zeros(iterations_avant_cycle)
+    dans  = np.zeros(iterations_dans_cycle)
+    tot = np.zeros(iterations_avant_cycle + iterations_dans_cycle)
+    tot[0] = u0
+    for i in range(1,len(tot)):
+        tot[i] = f(tot[i-1],k)
+    return tot[:iterations_avant_cycle],tot[iterations_avant_cycle:]
+
+fig = plt.figure()
+feigenbaum = fig.gca()
+
+for k in k_list:
+    avant,dans = get_cycle(k,u0)
+    feigenbaum.plot([k]*len(dans),dans,'.k',markersize=1)
+
+if ylim: feigenbaum.set_ylim(ylim)
+feigenbaum.set_xlim(kmin,kmax)
+fig.savefig('PNG/misc_feigenbaum.png')
 
 
 
