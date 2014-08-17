7d6857f07104c9fdf64eb679c551a69f4072afb8
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2014-08-17 15:02:34 +0200

    Démarrage fichier Feigenbaum

---
+++ lib/misc_feigenbaum.py
@@ -0,0 +1,26 @@
+# coding: latin1
+
+# Sauf mention explicite du contraire par la suite, ce travail a été fait par 
+# Jean-Julien Fleck, professeur de physique/IPT en PCSI1 au lycée Kléber. 
+# Vous êtes libres de le réutiliser et de le modifier selon vos besoins.
+# 
+# Si l'encodage vous pose problème, vous pouvez réencoder le fichier à l'aide 
+# de la commande
+# 
+# recode l1..utf8 monfichier.py
+# 
+# Il faudra alors modifier la première ligne en # coding: utf8
+# pour que Python s'y retrouve.
+
+
+
+""" 
+Encore une illustration tirée du livre "Dieu joue-t-il aux dés ?" de Ian 
+Stewart pour illustrer les doublements de périodes successifs dans les cycles 
+limites de suites récursives à partir de l'application logistique 
+x -> k*x*(1-x).
+"""
+
+
+
+
