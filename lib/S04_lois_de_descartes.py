bf9a1bcb0f9db279c3da1c32466f24f5c0a749ab
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2014-10-20 11:12:28 +0200

    Ajout de commentaires

--- lib/S04_lois_de_descartes.py
+++ lib/S04_lois_de_descartes.py
@@ -15,6 +15,20 @@
 
 
 
+"""
+
+Petite animation turtle pour illustrer les lois de Descartes pour la 
+réfraction. L'idée est de simuler la "classique" illustration par envoi d'un 
+faisceau lumineux sur un hémicylindre de plexiglas et d'observer la direction 
+prise par le rayon réfracté.
+
+On va colorer différemment les rayons tous les 10 degrés pour suivre que le 
+"tassement" s'observe principalement lorsque les angles incidents sont presque 
+à la perpendiculaire de la normale.
+
+"""
+
+
 from math import *
 from turtle import *
 
@@ -30,34 +44,35 @@ circle(R,180)           # On trace le cercle sur 180 degrés
 up()                    # On soulève
 goto(-200,0)            # Pour aller tracer l'axe optique 
 right(180)              # Retour dans l'axe
-for i in range(20):
-    down()                  # On pose le stylo
-    forward(10)            # et tracé effectif
-    up()
-    forward(10)
+for i in range(20):     # On va tracer la normale en pointillés
+    down()              # On pose le stylo,
+    forward(10)         # on avance en traçant,
+    up()                # on relève le stylo,
+    forward(10)         # on avance sans tracer
 
-n = 2.0
-speed(10)
-COLORS = ['red','blue']
-color('red')
-radians()
-for i in range(1,91):
-    if i > 80: speed(2)
-    if i%10 == 9: color(COLORS[(i//10+1)%2])
-    i = pi*i/180
-    r = asin(sin(i)/n)
-    up()
+n = 2.0                 # Indice du plexiglas
+speed(10)               # On accélère un peu
+COLORS = ['red','blue'] # Les deux couleurs tous les 10° incidents
+color('red')            # On commence sur le rouge
+radians()               # On passe en radians (pour turtle) pour utiliser sin et cos
+for i in range(1,91):   # Un rayon tous les degrés
+    if i > 80: speed(2) # Si on est proche de la fin, on ralentit un peu
+    if i%10 == 9: color(COLORS[(i//10+1)%2]) # Changement de couleur
+    i = pi*i/180        # Conversion en radians
+    r = asin(sin(i)/n)  # Calcul de l'angle réfracté
+    up()                # On soulève le stylo pour se placer au bon endroit
     goto(-2*R*cos(i),-2*R*sin(i))
-    left(i)
-    down()
-    forward(2*R)
-    right(i-r)
-    forward(2*R)
-    up()
-    right(r)
+    left(i)             # On prend la bonne direction
+    down()              # On pose le stylo
+    forward(2*R)        # jusqu'à atteindre l'interface
+    right(i-r)          # Là, on est réfracté
+    forward(2*R)        # et on continue notre route
+    up()                # On se relève
+    right(r)            # et on se remet dans l'axe.
 
+
+# À la fin, on attend 10s que l'orateur puisse expliquer aux élèves.
 import time
-
 time.sleep(10)
 
 
