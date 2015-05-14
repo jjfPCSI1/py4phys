3725b54239401687746f9724d48abce399112281
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2015-05-14 15:05:00 +0200

    Refonte du programme pour s'affranchir de la base fournie par CoolProp qui pose des soucis d'unités. Fonctionne avec CoolProp5, pas de certitudes pour CoolProp4

--- lib/T6_diagramme_Ph_coolprop.py
+++ lib/T6_diagramme_Ph_coolprop.py
@@ -8,6 +8,7 @@
 
 """
 Fabrication d'un diagramme (P,h) avec les iso-choses adéquates.
+Reprise complète car c'est galère de se baser sur le Ph_plot fourni par CoolProp.
 """
 
 import numpy as np               # Les outils mathématiques
@@ -19,21 +20,48 @@ import matplotlib.pyplot as plt  # Les outils graphiques
 print(CP.FluidsList())           # Pour regarder les fluides disponibles
 fluide= 'Water'                  # Le choix du fluide
 Plogscale = True                 # Axe en pression logarithmique ?
-iso_T = True                     # Veut-on des isothermes ?
-iso_x = True                     # et les isotitres ?
-iso_s = True                     # et les isentropiques ?
+iso_T = True                    # Veut-on des isothermes ?
+iso_x = True                    # et les isotitres ?
+iso_s = False                    # et les isentropiques ?
 iso_v = True                     # et les isochores ?
 
-# Données pour les isothermes
+# Les unités dans lesquelles on veut travailler (voir CONVERSION)
+unitP = 'bar'
+unitH = 'kJ'
+unitT = 'celsius'
+
+# Les utilitaires pour les conversions
+if unitT == 'celsius':
+    def kelvin(t): return t + 273.15  # Conversion en kelvin
+    def celsius(T):return T - 273.15  # Conversion en celsius
+else: # Si on reste en kelvin, les transformations sont l'identité
+    def kelvin(t): return t
+    def celsius(t): return t
+CONVERSION = {'bar': 1e5, 'kPa': 1e3, 'Pa': 1, 'kJ': 1e3, 'J': 1}
+
+# Les limites du graphique (en unités du graphique)
+# On part de la pression triple (+ 1Pa pour être sûr)
+Pmin = (CP.PropsSI(fluide,'ptriple')+1)/CONVERSION[unitP]
+# On va jusqu'à un peu au-dessus du point critique
+Pmax = (1.5*CP.PropsSI(fluide,'pcrit'))/CONVERSION[unitP]
+# On prend l'enthalpie à gauche un peu sous le point triple côté liquide
+Hmin = 0.9*CP.PropsSI('H','P',Pmin*CONVERSION[unitP],'Q',0,fluide)/CONVERSION[unitH]
+# et l'enthalpie à droite un peu au-dessus de celle du point triple côté gaz
+Hmax = 1.5*CP.PropsSI('H','P',Pmin*CONVERSION[unitP],'Q',1,fluide)/CONVERSION[unitH]
+
+# Données pour les isothermes (on veut une représentation sympa en celsius en pratique)
 dT = 20                                 # Incrément de températures
 Ttriple = CP.PropsSI(fluide,'Ttriple')  # Valeur de la température au point triple
 Tcrit = CP.PropsSI(fluide,'Tcrit')      # et au point critique
-Tmin = int(Ttriple/10)*10 + 10          # Par défaut, on par près du point triple
+# Par défaut, on part près du point triple avec une valeur arrondi à une 
+# dizaine en celsius.
+Tmin = kelvin(int(celsius(Ttriple)/10)*10 + 10) + 20
 val_T = np.arange(Tmin,1.5*Tcrit,dT)    # et on dépasse un peu le point critique
 T_to_show = list(range(2,len(val_T),2)) # Sélection des T à afficher (mettre None pour toutes)
 
 # Données pour les isotitres
 val_x = np.linspace(0.1,0.9,9)          # Les valeurs des isotitres
+x_to_show = None
 
 # Données pour les isentropiques
 ds = 0.5e3
@@ -53,6 +81,7 @@ v_to_show = None                                       # On les affiche toutes.
 
 # Quelques constantes
 UNITS = {'T': 'K', 'Q': '', 'S': 'kJ/K/kg', 'V': 'm$^3$/kg'}
+if unitT == 'celsius': UNITS['T'] = '$^{\circ}$C'
 LABEL = {'T': 'T', 'Q': 'x','S': 's', 'V': 'v'}
 COLOR_MAP = {'T': 'Darkred',
              'P': 'DarkCyan',
@@ -60,6 +89,11 @@ COLOR_MAP = {'T': 'Darkred',
              'V': 'DarkBlue',
              'S': 'DarkOrange',   
              'Q': 'black'}
+LINE_STYLE = {'T': '-.',
+             'V': '--',
+             'S': ':',   
+             'Q': '-'}
+          
 
 # On prépare un format pour impression sur A3 ou presque (dimensions en pouces)
 plt.figure(figsize=(30,21))
@@ -91,22 +125,21 @@ def place_label(x,y,label,indice=None,cotan=False,color='k'):
     if cotan:                            # Si on dépasse la verticale
         rot = 90 - np.degrees(np.arctan(1/a))
     t = plt.text(x[N],y[N],label,        # On met le texte au bon endroit
-    ha='center',va='center',color=color,rotation = rot) # Avec la bonne rotation
+    ha='center',va='center',color=color,rotation = str(rot)) # Avec la bonne rotation
     # On se débrouille pour que la "boîte" d'écriture soit semi-transparente
-    t.set_bbox(dict(facecolor='w',edgecolor='None',alpha=0.8))
+    #t.set_bbox(dict(facecolor='w',edgecolor='None',alpha=0.8))
+    t.set_bbox(dict(boxstyle="round",facecolor='w',edgecolor='None',alpha=0.85))
 
 def fait_isolignes(type,valeurs,position=None,nb_points=1000,to_show=None,round_nb = 0 ):
     """ S'occupe du calcul et du tracé des isolignes. """
     if not(to_show):                        # Valeurs par défauts:
         to_show = list(range(len(valeurs))) # toutes !
-    Pmin,Pmax = plt.ylim()                  # On regarde les 
-    Hmin,Hmax = plt.xlim()                  # limites du graphique
-    # Il y a un bug au niveau des unités du graphe et des points de 
-    # représentation, d'où les 1e3 qui trainent un peu partout
+    Pmin,Pmax = [p*CONVERSION[unitP] for p in plt.ylim()]# On regarde les 
+    Hmin,Hmax = [H*CONVERSION[unitH] for H in plt.xlim()]# limites du graphique
     # Par défaut, l'échantillonnage en P est linéaire
-    val_P = np.linspace(Pmin*1e3,Pmax*1e3,nb_points) 
-    # Sinon, on se met en échelle log. (1e3 -> 3)
-    if Plogscale: val_P = 3+np.logspace(np.log10(Pmin),np.log10(Pmax),nb_points)
+    val_P0 = np.linspace(Pmin,Pmax,nb_points) 
+    # Sinon, on se met en échelle log. 
+    if Plogscale: val_P0 = np.logspace(np.log10(Pmin),np.log10(Pmax),nb_points)
     # Cas où les lignes ne vont pas sur tout l'éventail des pression, on 
     # échantillonne en températures (car on ne peut pas directement 
     # échantillonner en enthalpie h)
@@ -118,40 +151,48 @@ def fait_isolignes(type,valeurs,position=None,nb_points=1000,to_show=None,round_
         if type == 'V':  # Cas particulier des volumes massiques: échantillonnage
             val_P = CP.PropsSI('P','T',val_T,'D',1/val,fluide)  # en température
             val_H = CP.PropsSI('H','T',val_T,'D',1/val,fluide)  # et non en P
+        elif type == 'Q':
+            val_T = np.linspace(Ttriple,Tcrit,nb_points)
+            val_P = CP.PropsSI('P','T',val_T,'Q',val,fluide)
+            val_H = CP.PropsSI('H','T',val_T,'Q',val,fluide)
         else:            # Sinon, on utilise l'éventail des pression
+            val_P = np.array(val_P0)
             val_H = CP.PropsSI('H','P',val_P,type,val,fluide)
+        # On se remet dans les bonnes unités
+        val_H = val_H/CONVERSION[unitH]
+        val_P = val_P/CONVERSION[unitP]
         if type == 'S': val /= 1e3 # Pour mettre en kJ/K/kg
+        if type == 'T': val = celsius(val)
         if round_nb >0 : val = str(round(val,round_nb)) # Pour faire joli
         else: val = str(int(round(val)))                # là aussi...
         label = '${}={}$ {}'.format(LABEL[type],val,UNITS[type])
-        plt.plot(val_H,val_P,color=COLOR_MAP[type])     # Affichage courbe
+        # Affichage courbe
+        plt.plot(val_H,val_P,
+                 color=COLOR_MAP[type],linestyle=LINE_STYLE[type])     
         if i in to_show: # Ainsi que du label s'il fait partie de la liste
             place_label(val_H,val_P,label,int(position*nb_points))
 
 # Le programme proprement dit commence ici.
 
-ph_plot = CPP.PropsPlot(fluide,'Ph')   # On demande gentiment le plot de base
-ph_plot._draw_graph()                  # On s'assure qu'il a discuté avec plt
+plt.figure(figsize=(15,10))
+
+plt.xlim(Hmin,Hmax)
+plt.ylim(Pmin,Pmax)
 if Plogscale: plt.yscale('log')        # Passage en log(P)
 
-if iso_x: # Les lignes isotitres sont un peu spéciales, donc ont leur code propre
-    ph_plot.draw_isolines('Q',val_x)   # Tracé des lignes isotitres
-    # Récupération de la liste des isotitres.
-    isoQ = CPP.Plots.IsoLines(fluide,'Ph','Q').get_isolines(val_x)
-    for line in isoQ:                  # Rajout des label
-        label = line['label'] + line['unit']
-        x,y = line['x'],line['y']
-        place_label(x,y,label,indice=len(x)//20)
-else: # On trace tout de même quelque chose (de déjà présent) pour s'assurer
-    ph_plot.draw_isolines('Q',[0,1],num=2)# une bonne sélection des bornes
-
+# Tracé de la courbe de saturation
+fait_isolignes('Q',[0,1],position=0,to_show=[''])
 # Ici, on fait toutes les autres isolignes (le boulot a été fait plus haut)
 if iso_T: fait_isolignes('T',val_T,position=0.8,to_show=T_to_show)
 if iso_s: fait_isolignes('S',val_s,position=0.3,to_show=s_to_show,round_nb=3)
 if iso_v: fait_isolignes('V',val_v,position=0.25,to_show=v_to_show,round_nb=3)
+if iso_x: fait_isolignes('Q',val_x,position=0.1,to_show=x_to_show,round_nb=2)
+
+plt.xlabel('Enthalpie massique $h$ (en {}/kg)'.format(unitH))
+plt.ylabel('Pression P (en {})'.format(unitP))
+plt.title('Diagramme Pression/Enthalpie pour le fluide {}'.format(fluide))
 
 plt.grid(which='both') # Rajout de la grille
-ph_plot._draw_graph()  # On oblige le dessin avant la sauvegarde
 plt.savefig('PNG/T6_diagramme_Ph_coolprop_{}.png'.format(fluide))
 
 
