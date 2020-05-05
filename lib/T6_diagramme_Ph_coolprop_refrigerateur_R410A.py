4855c404944e29f18be239c305effbf2e3ebcb70
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2020-05-05 22:23:20 +0200

    :bug: Retour des isentropiques et des lignes brisées.

--- lib/T6_diagramme_Ph_coolprop_refrigerateur_R410A.py
+++ lib/T6_diagramme_Ph_coolprop_refrigerateur_R410A.py
@@ -1,7 +1,7 @@
 # coding: utf8
 
-# Sauf mention explicite du contraire par la suite, ce travail a été fait par 
-# Jean-Julien Fleck, professeur de physique/IPT en PCSI1 au lycée Kléber. 
+# Sauf mention explicite du contraire par la suite, ce travail a été fait par
+# Jean-Julien Fleck, professeur de physique/IPT en PCSI1 au lycée Kléber.
 # Vous êtes libres de le réutiliser et de le modifier selon vos besoins.
 
 
@@ -24,7 +24,7 @@ fluide= 'R410A'                  # Le choix du fluide
 Plogscale = True                 # Axe en pression logarithmique ?
 iso_T = True                    # Veut-on des isothermes ?
 iso_x = False                    # et les isotitres ?
-iso_s = False                    # et les isentropiques ?
+iso_s = True                    # et les isentropiques ?
 iso_v = False                     # et les isochores ?
 
 # Les unités dans lesquelles on veut travailler
@@ -44,7 +44,7 @@ CONVERSION = {'bar': 1e5, 'kPa': 1e3, 'Pa': 1, 'kJ': 1e3, 'J': 1}
 # Les limites du graphique (en unités du graphique)
 # On part de la pression triple (+ 1Pa pour être sûr)
 #Pmin = (CP.PropsSI(fluide,'ptriple')+1)/CONVERSION[unitP]
-Pmin = 0.5  
+Pmin = 0.5
 # On va jusqu'à un peu au-dessus du point critique
 #Pmax = (1.5*CP.PropsSI(fluide,'pcrit'))/CONVERSION[unitP]
 Pmax = 50
@@ -57,7 +57,7 @@ Hmax = 1.5*CP.PropsSI('H','P',Pmin*CONVERSION[unitP],'Q',1,fluide)/CONVERSION[un
 dT = 10                                 # Incrément de températures
 Ttriple = CP.PropsSI(fluide,'Ttriple')  # Valeur de la température au point triple
 Tcrit = CP.PropsSI(fluide,'Tcrit')      # et au point critique
-# Par défaut, on part près du point triple avec une valeur arrondi à une 
+# Par défaut, on part près du point triple avec une valeur arrondi à une
 # dizaine en celsius.
 #Tmin = kelvin(int(celsius(Ttriple)/10)*10 + 10) + 20
 Tmin = kelvin(-60)
@@ -94,24 +94,24 @@ COLOR_MAP = {'T': 'Darkred',
              'P': 'DarkCyan',
              'H': 'DarkGreen',
              'V': 'DarkBlue',
-             'S': 'DarkOrange',   
+             'S': 'DarkOrange',
              'Q': 'black'}
 LINE_STYLE = {'T': '-.',
              'V': ':',
-             'S': '--',   
+             'S': '--',
              'Q': '-'}
-          
+
 
 # On prépare un format pour impression sur A3 ou presque (dimensions en pouces)
 #plt.figure(figsize=(30,21))
 plt.figure(figsize=(16.54,11.69))
 
 def place_label(x,y,label,indice=None,cotan=False,color='k'):
-    """ Routine qui se débrouille pour mettre un label semi-transparent au 
-    niveau de la courbe données par ses coordonnées x et y. Si on sait que le 
-    label sera presque vertical avec possibilité de dépasser 90°, on peut 
-    utiliser cotan=True pour corriger (considération purement esthétique). 
-    'indice' correspond à la position dans les tableaux x et y où devra 
+    """ Routine qui se débrouille pour mettre un label semi-transparent au
+    niveau de la courbe données par ses coordonnées x et y. Si on sait que le
+    label sera presque vertical avec possibilité de dépasser 90°, on peut
+    utiliser cotan=True pour corriger (considération purement esthétique).
+    'indice' correspond à la position dans les tableaux x et y où devra
     s'afficher le label demandé. """
     print(x[0],y[0],label) # un peu de feedback pour savoir ce qu'on calcule
     N = len(x)//2          # Emplacement par défaut
@@ -119,7 +119,7 @@ def place_label(x,y,label,indice=None,cotan=False,color='k'):
     xi,xf = plt.xlim()     # Les limites en x du graphe
     yi,yf = plt.ylim()     # Pareil en y
     Xsize = xf - xi        # La largeur
-    # Pour la hauteur et la pente, cela dépend si les ordonnées sont en repère 
+    # Pour la hauteur et la pente, cela dépend si les ordonnées sont en repère
     # logarithmique ou non.
     if Plogscale:
         Ysize = np.log10(yf) - np.log10(yi)
@@ -128,7 +128,7 @@ def place_label(x,y,label,indice=None,cotan=False,color='k'):
         Ysize = yf - yi
         a = (y[N+1]-y[N-1])/(x[N+1]-x[N-1]) * Xsize/Ysize
     bbox = plt.gca().get_window_extent() # Récupération de la taille de la figure
-    a *= bbox.height / bbox.width        # Correction de la pente avec la taille 
+    a *= bbox.height / bbox.width        # Correction de la pente avec la taille
     rot = np.degrees(np.arctan(a))       # Calcul de l'angle de rotation
     if cotan:                            # Si on dépasse la verticale
         rot = 90 - np.degrees(np.arctan(1/a))
@@ -142,19 +142,19 @@ def fait_isolignes(type,valeurs,position=None,nb_points=1000,to_show=None,round_
     """ S'occupe du calcul et du tracé des isolignes. """
     if not(to_show):                        # Valeurs par défauts:
         to_show = list(range(len(valeurs))) # toutes !
-    Pmin,Pmax = [p*CONVERSION[unitP] for p in plt.ylim()]# On regarde les 
+    Pmin,Pmax = [p*CONVERSION[unitP] for p in plt.ylim()]# On regarde les
     Hmin,Hmax = [H*CONVERSION[unitH] for H in plt.xlim()]# limites du graphique
     # Par défaut, l'échantillonnage en P est linéaire
-    val_P0 = np.linspace(Pmin,Pmax,nb_points) 
-    # Sinon, on se met en échelle log. 
+    val_P0 = np.linspace(Pmin,Pmax,nb_points)
+    # Sinon, on se met en échelle log.
     if Plogscale: val_P0 = np.logspace(np.log10(Pmin),np.log10(Pmax),nb_points)
-    # Cas où les lignes ne vont pas sur tout l'éventail des pression, on 
-    # échantillonne en températures (car on ne peut pas directement 
+    # Cas où les lignes ne vont pas sur tout l'éventail des pression, on
+    # échantillonne en températures (car on ne peut pas directement
     # échantillonner en enthalpie h)
     Tmin = Ttriple
     Tmax = CP.PropsSI('T','P',Pmax,'H',Hmax,fluide)
     val_T = np.linspace(Tmin,Tmax,nb_points)
-    # Pour chacune des valeurs demandées, 
+    # Pour chacune des valeurs demandées,
     for val,i in zip(valeurs,range(len(valeurs))):
         if type == 'V':  # Cas particulier des volumes massiques: échantillonnage
             val_P = CP.PropsSI('P','T',val_T,'D',1/val,fluide)  # en température
@@ -184,8 +184,8 @@ def fait_isolignes(type,valeurs,position=None,nb_points=1000,to_show=None,round_
         else: val = str(int(round(val)))                # là aussi...
         label = '${}={}${}'.format(LABEL[type],val,UNITS[type])
         # Affichage courbe
-        plt.plot(val_H,val_P,'-',linewidth=2,
-                 color=COLOR_MAP[type],linestyle=LINE_STYLE[type])     
+        plt.plot(val_H,val_P,linewidth=2,
+                 color=COLOR_MAP[type],linestyle=LINE_STYLE[type])
         if i in to_show: # Ainsi que du label s'il fait partie de la liste
             place_label(val_H,val_P,label,int(position*nb_points))
 
@@ -215,7 +215,7 @@ if iso_x: fait_isolignes('Q',val_x,position=0.1,to_show=x_to_show,round_nb=2)
 mettre_frigo = True
 
 def place_point(H,P,S,T):
-    """ Prend un point en coordonnées SI pour le placer sur le graphe en 
+    """ Prend un point en coordonnées SI pour le placer sur le graphe en
     coordonnées du graphe. """
     t = int(P/CONVERSION[unitP]),int(H/1e3),round(S/1e3,2),int(round(celsius(T))),0
     plt.plot(H/CONVERSION[unitH],P/CONVERSION[unitP],'ko',markersize=8,
@@ -260,7 +260,7 @@ if mettre_frigo:
     H = CP.PropsSI('H','P',P,'Q',1,fluide)
     S = CP.PropsSI('S','P',P,'Q',1,fluide)
     place_point(H,P,S,T)
-    
+
 if False:
     # Le sécheur surchauffeur le remet jusqu'à 267°C (la pression baisse un peu)
     P = 9.4e5
@@ -299,7 +299,7 @@ Fessenheim = [(67.5,220),
               (0.07,40),  # Après condenseur
               (9,170),   # Sortie réservoir
               (71.5,227)]  # Sortie TurboPompe
-    
+
 
 plt.xlabel('Enthalpie massique $h$ (en {}/kg)'.format(unitH))
 plt.ylabel('Pression P (en {})'.format(unitP))
@@ -307,6 +307,3 @@ plt.title('Diagramme Pression/Enthalpie pour le fluide {}'.format(fluide))
 
 plt.grid(which='both') # Rajout de la grille
 plt.savefig('PNG/T6_diagramme_Ph_coolprop_refrigerateur_{}.pdf'.format(fluide))
-
-
-
