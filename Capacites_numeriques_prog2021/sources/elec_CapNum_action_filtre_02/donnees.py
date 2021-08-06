39d900b532ec0b4b64ac518d983128f4b0543dff
JJ Fleck <jeanjulien.fleck@gmail.com>
2021-08-06 10:20:08 +0200

    :children_crossing: Meilleure valeur de Q

--- Capacites_numeriques_prog2021/sources/elec_CapNum_action_filtre_02/donnees.py
+++ Capacites_numeriques_prog2021/sources/elec_CapNum_action_filtre_02/donnees.py
@@ -5,15 +5,15 @@ freqs, FFT = np.loadtxt('donnees.txt', unpack = True)
 FFT = FFT[freqs < 1000]
 freqs = freqs[freqs < 1000]
 
-def H_PBas(f, Q=100):
+def H_PBas(f, Q=10):
     """
     Fonction de transfert de passe-bas
     à valeur complexe qui prend une fréquence en argument
     """
-    f0 = 0.45e3
+    f0 = 0.25e3
     return 1 / ( 1 + 1j * f / (Q * f0) - (f / f0)**2)
 
-def H_PBande(f, Q=100):
+def H_PBande(f, Q=30):
     """
     Fonction de transfert de passe-bande à amplification 
     à valeur complexe qui prend une fréquence en argument
