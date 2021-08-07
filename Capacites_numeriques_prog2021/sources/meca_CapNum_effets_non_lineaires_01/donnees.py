45979165ad52af18636a255f89bc64fbb826316c
JJ Fleck <jeanjulien.fleck@gmail.com>
2021-08-07 22:49:43 +0200

    :bug: Une amplitude nulle ne mène à rien

--- Capacites_numeriques_prog2021/sources/meca_CapNum_effets_non_lineaires_01/donnees.py
+++ Capacites_numeriques_prog2021/sources/meca_CapNum_effets_non_lineaires_01/donnees.py
@@ -3,5 +3,5 @@ import numpy as np
 
 g = 9.81
 ell = 1.0
-liste_theta0 = [10 * i for i in range(18)] # en degrés !
+liste_theta0 = [10 * (i+1) for i in range(17)] # en degrés !
 t = np.linspace(0, 10, 1000)
